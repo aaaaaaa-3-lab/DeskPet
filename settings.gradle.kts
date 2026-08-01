dee29aad00101646ed7e2cef8386b0f0dadf1965
aaaaaaa-3-lab <1573271992@qq.com>
2026-08-02 02:02:36 +0800

    bare gradle: no kotlin, no ax deps

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,17 +1,17 @@
 pluginManagement {
     repositories {
-        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://mirrors.cloud.tencent.com/nexus/repository/maven-public/") }
         maven { url = uri("https://maven.aliyun.com/repository/public") }
         maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
+        gradlePluginPortal()
         google()
         mavenCentral()
-        gradlePluginPortal()
     }
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://mirrors.cloud.tencent.com/nexus/repository/maven-public/") }
         maven { url = uri("https://maven.aliyun.com/repository/public") }
         google()
         mavenCentral()
