19e195d5683fc0264041bc5972da9681b527f929
aaaaaaa-3-lab <1573271992@qq.com>
2026-08-02 22:50:09 +0800

    v4.3 vc12 full push

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.vaelky.deskpet"
         minSdk = 26
         targetSdk = 34
-        versionCode = 11
-        versionName = "4.0"
+        versionCode = 12
+        versionName = "4.3"
     }
 
     buildTypes {
